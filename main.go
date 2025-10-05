dcd0f381ca905001f575a8efd42cc38a58f2a4af
Nilcs2 <nil.cs2@gmail.com>
2025-10-05 16:35:16 -0700

    Added indirect module in my project

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"github.com/urfave/cli/v2"
+)
+
 
 func main() {
-	fmt.Println("My first Go script")
+	app := &cli.App{
+		Action: func(cCtx *cli.context) error {
+			fmt.Println("Hello %q", cCtx.Args().Get(0))
+			return nil
+		},
+	}
+	
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
 
